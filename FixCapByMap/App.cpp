525119fbf4ee66929af4bb738186c019e4fc7e00
Boar Victor <117311241+Rotciv004@users.noreply.github.com>
2024-11-19 11:13:37 +0200

    Add files via upload

---
+++ FixCapByMap/App.cpp
@@ -0,0 +1,21 @@
+#include "ExtendedTest.h"
+#include "ShortTest.h"
+
+#include "FixedCapBiMap.h"
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	//testAll();
+	//testAllExtended();
+	myTestAll();
+
+	cout << "That's all!" << endl;
+	system("pause");
+	return 0;
+}
+
+
