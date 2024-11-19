ff5505bca754e979ce5e4588e5c40e45bff9478e
Boar Victor <117311241+Rotciv004@users.noreply.github.com>
2024-11-19 11:09:40 +0200

    Add files via upload

---
+++ Multi_Map/MultiMap.h
@@ -0,0 +1,61 @@
+#pragma once
+#include<vector>
+#include<utility>
+//DO NOT INCLUDE MultiMapIterator
+
+using namespace std;
+
+//DO NOT CHANGE THIS PART
+typedef int TKey;
+typedef int TValue;
+typedef std::pair<TKey, TValue> TElem;
+#define NULL_TVALUE -111111
+#define NULL_TELEM pair<int,int>(-111111, -111111)
+class MultiMapIterator;
+
+class Node {
+public:
+	TElem data;
+	Node* next;
+
+	Node(const TElem& elem) : data(elem), next(nullptr) {}
+};
+
+class MultiMap
+{
+	friend class MultiMapIterator;
+
+private:
+
+	Node* head;
+	int length;
+
+public:
+	//constructor
+	MultiMap() : head(nullptr), length(0) {}
+
+	//adds a key value pair to the multimap
+	void add(TKey c, TValue v);
+
+	//removes a key value pair from the multimap
+	//returns true if the pair was removed (if it was in the multimap) and false otherwise
+	bool remove(TKey c, TValue v);
+
+	//returns the vector of values associated to a key. If the key is not in the MultiMap, the vector is empty
+	vector<TValue> search(TKey c) const;
+
+	//returns the number of pairs from the multimap
+	int size() const;
+
+	//checks whether the multimap is empty
+	bool isEmpty() const;
+
+	//returns an iterator for the multimap
+	MultiMapIterator iterator() const;
+
+	//descturctor
+	~MultiMap();
+
+	vector<TValue> removeKey(TKey key);
+};
+
